e29ec79684b3965ce3b0bd7fc2cf3dd653a9a43d
XuJiang <j10047392@gmail.com>
2025-04-29 12:50:45 -0400

    Add sale paragraph and conclusion
    1. Sale paragraph
    2. Conclusion

--- JS/scrolly.js
+++ JS/scrolly.js
@@ -14,8 +14,8 @@ function scroll(n, offset, func1, func2){
 new scroll('div1', '75%', graph1, graphClean);  //open graph 1
 new scroll('div2', '75%', graph2, graph1);  //transit to graph 2
 new scroll('div3', '75%', graph3, graph2);  //transit to graph 3
-new scroll('div4', '75%', graph4, graph3);  //transit to graph 4
-new scroll('div5', '75%', graphClean, graph4);
+new scroll('div4', '75%', graphClean, graphClean);
+new scroll('div5', '75%', graphClean, graphClean);
 function graphClean(){
     //grab svg element
     let svg = d3.select("#canvas");
